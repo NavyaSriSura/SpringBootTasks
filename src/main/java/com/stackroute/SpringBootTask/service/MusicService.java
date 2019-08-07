c5879a8abc17875e65e84188601dc911f4328639
NavyaSriSura <35621399+NavyaSriSura@users.noreply.github.com>
2019-08-07 15:08:00 +0530

    Modified method return types

--- src/main/java/com/stackroute/SpringBootTask/service/MusicService.java
+++ src/main/java/com/stackroute/SpringBootTask/service/MusicService.java
@@ -5,16 +5,15 @@ import com.stackroute.SpringBootTask.domain.Music;
 import java.util.List;
 
 public interface MusicService {
+    public Music saveMusic(Music music);
 
-    public Music saveTrack(Music music);
-
-    public List<Music> getTrack();
+    public List<Music> getMusic();
 
     public Music getById(int id);
 
-    public void deleteById(int id);
+    public Music deleteById(int id);
 
-    public boolean updateById(Music music, int id);
+    public Music updateById(Music music, int id);
 
     public List<Music> getByName(String name);
 }
