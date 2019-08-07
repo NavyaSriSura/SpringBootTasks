787ee5d39c842e167af9c85d1ceafcf0bdd780e4
NavyaSriSura <navyasura98@gmail.com>
2019-08-07 16:36:51 +0530

    solved global issues

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpringBootTasks
+
+Add @Lombok
---
+++ src/main/java/com/stackroute/SpringBootTask/service/MusicService.java
@@ -0,0 +1,21 @@
+package com.stackroute.SpringBootTask.service;
+
+import com.stackroute.SpringBootTask.domain.Music;
+import com.stackroute.SpringBootTask.exceptions.TrackAlreadyExistsException;
+import com.stackroute.SpringBootTask.exceptions.TrackNotFoundException;
+
+import java.util.List;
+
+public interface MusicService {
+    public Music saveMusic(Music music) throws TrackAlreadyExistsException;
+
+    public List<Music> getMusic();
+
+    public Music getById(int id) throws TrackNotFoundException;
+
+    public Music deleteById(int id);
+
+    public Music updateById(Music music, int id);
+
+    public List<Music> getByName(String name);
+}
