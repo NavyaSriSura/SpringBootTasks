035bfda4d937275bc2a98e20f01bb0b132a6e337
NavyaSriSura <35621399+NavyaSriSura@users.noreply.github.com>
2019-08-08 09:44:21 +0530

    solved resource mapping, encapsulation issues

--- src/main/java/com/stackroute/SpringBootTask/service/MusicService.java
+++ src/main/java/com/stackroute/SpringBootTask/service/MusicService.java
@@ -1,17 +1,17 @@
 package com.stackroute.SpringBootTask.service;
 
 import com.stackroute.SpringBootTask.domain.Music;
-import com.stackroute.SpringBootTask.exceptions.TrackAlreadyExistsException;
-import com.stackroute.SpringBootTask.exceptions.TrackNotFoundException;
+import com.stackroute.SpringBootTask.exceptions.MusicAlreadyExistsException;
+import com.stackroute.SpringBootTask.exceptions.MusicNotFoundException;
 
 import java.util.List;
 
 public interface MusicService  {
-    public Music saveMusic(Music music) throws TrackAlreadyExistsException;
+    public Music saveMusic(Music music) throws MusicAlreadyExistsException;
 
     public List<Music> getMusic();
 
-    public Music getById(int id) throws TrackNotFoundException;
+    public Music getById(int id) throws MusicNotFoundException;
 
     public Music deleteById(int id);
 
