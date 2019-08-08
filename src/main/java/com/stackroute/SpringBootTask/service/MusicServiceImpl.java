4b0fb4ea819e36064f170982389437bc5ede43fa
NavyaSriSura <35621399+NavyaSriSura@users.noreply.github.com>
2019-08-08 10:01:50 +0530

    solved encapsulation issue

--- src/main/java/com/stackroute/SpringBootTask/service/MusicServiceImpl.java
+++ src/main/java/com/stackroute/SpringBootTask/service/MusicServiceImpl.java
@@ -1,8 +1,14 @@
 package com.stackroute.SpringBootTask.service;
 
 import com.stackroute.SpringBootTask.domain.Music;
+import com.stackroute.SpringBootTask.exceptions.MusicAlreadyExistsException;
+import com.stackroute.SpringBootTask.exceptions.MusicNotFoundException;
 import com.stackroute.SpringBootTask.repository.MusicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +16,20 @@ import java.util.Optional;
 
 @Service
 public class MusicServiceImpl implements MusicService {
-    private String name1;
+
+
+
+   private String name1;
+
+   private int rating1;
+
     private String comments1;
-    private int rating1;
-    private String name2;
-    private String comments2;
-    private int rating2;
+
+   private String name2;
+
+   private int rating2;
+
+   private String comments2;
     private MusicRepository musicRepository;
 
     @Autowired
@@ -32,7 +46,7 @@ public class MusicServiceImpl implements MusicService {
     }
 
     @Override
-    public Music saveMusic(Music music) {
+    public Music saveMusic(Music music) throws MusicAlreadyExistsException {
 
 
         Music savedMusic = musicRepository.save(music);
@@ -46,12 +60,11 @@ public class MusicServiceImpl implements MusicService {
     }
 
     @Override
-    public Music getById(int id) {
+    public Music getById(int id) throws MusicNotFoundException {
 
         Optional<Music> user_id = musicRepository.findById(id);
         return user_id.get();
     }
-
 
     @Override
     public Music deleteById(int id) {
@@ -74,7 +87,9 @@ public class MusicServiceImpl implements MusicService {
 
     @Override
     public List<Music> getByName(String name) {
-        return null;
+        List<Music> user_id = musicRepository.findTitleByName(name);
+
+        return user_id;
     }
 
 
